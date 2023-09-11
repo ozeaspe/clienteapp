f352754fefcc984519f3e91a5316402fdbb92981
Ozeas <ozeaspe@gmail.com>
2023-09-11 15:26:13 -0300

    Tela de Login
    Criando a tela de login parte 01

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,48 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './signin.css'
+
+import fisioterapia from '../../assets/fisioterapia.jpg'
 
 function SignIn(){
+
+    //Armazena os dados que o usuário digitar
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+   
+
     return(
-        <div>
-            <h1>Página login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={fisioterapia} alt='Logo do clienteapp' />
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+                    <input
+                    type='text'
+                    placeholder='email@email.com'
+                    //Usa a useState para capturar o que foi digitado
+                    value={email}
+                    //Capturar os dados digitados e passar para a useState
+                    onChange={ (e) => setEmail(e.target.value)}
+                    />
+
+                    <input
+                    type='password'
+                    placeholder='********'
+                    value={password}
+                    onChange={ (e) => setPassword(e.target.value)}
+                    />
+                    
+                    
+                </form>
+
+                <Link to="/register">Criar uma conta</Link>
+            </div>
         </div>
     )
 }
